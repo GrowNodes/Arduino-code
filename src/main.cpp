30e24a34834b38df57f452086fe98d57cb00f092
Kevin <keyeh@calpoly.edu>
2017-01-16 17:24:20 -0800

    responses

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,60 @@ dht DHT;
 PJON<SoftwareBitBang> MCUBus(45);   // device ID 45
 float air_temp_f = 0;
 unsigned long tempf_last_read = 0;
-//
-//
-//
+
+
+
+
+
+
+
+constexpr unsigned int str2int(const char* str, int h = 0)
+{
+    return !str[h] ? 5381 : (str2int(str, h+1) * 33) ^ str[h];
+}
+
+
+
+void set_grow_light(bool on) {
+  if (on) {
+    Serial.println("turn grow light on");
+    MCUBus.reply("ok", 2);
+    return;
+  }
+  Serial.println("turn grow light off");
+  MCUBus.reply("ok", 2);
+}
+
+void payload_router(const char* payload) {
+  switch (str2int(payload)) {
+    case str2int("grow_light_on"): {
+      set_grow_light(true);
+      break;
+    }
+
+    case str2int("grow_light_off"): {
+      set_grow_light(false);
+      break;
+    }
+
+    case str2int("air_temp_f"): {
+      String reply_str = "air_temp_f=";
+      reply_str += (float)random(6500, 9900)*0.01;
+      MCUBus.reply(reply_str.c_str(), reply_str.length());
+      break;
+    }
+
+    case str2int("water_level"): {
+      String reply_str = "water_level=";
+      reply_str += (float)random(100, 300)*0.01;
+      MCUBus.reply(reply_str.c_str(), reply_str.length());
+      break;
+    }
+  }
+}
+
+
+
 void setup() {
   Serial.begin(74880);
   Serial.println("Ready to receive");
@@ -36,6 +87,7 @@ void setup() {
     Serial.print(" bytes: ");
     Serial.println(payload_str);
 
+    payload_router(payload_str.c_str());
   });
 
 };
