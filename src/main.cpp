80e907d07f12b6d1a3aaa0b732f95c3f9123ce16
Kevin <keyeh@calpoly.edu>
2017-01-17 20:49:42 -0800

    rewrite

--- src/main.cpp
+++ src/main.cpp
@@ -15,64 +15,68 @@ PJON<SoftwareBitBang> MCUBus(45);   // device ID 45
 float air_temp_f = 0;
 unsigned long tempf_last_read = 0;
 
+void payloadRouter(const char* payload_str) {
 
+  if (strcmp_P(payload_str, (PGM_P)F("water_level")) == 0) {
+    char reply_str[] = "water_level=1.69";
+    Serial.print(F("Responding with: "));
+    Serial.println(reply_str);
+    MCUBus.reply(reply_str, sizeof(reply_str));
+    return;
+  }
 
+  if (strcmp_P(payload_str, (PGM_P)F("grow_light_on")) == 0) {
+    Serial.println(F("turn grow light on"));
+    MCUBus.reply("ok", 2);
+    return;
+  }
+
+  if (strcmp_P(payload_str, (PGM_P)F("grow_light_off")) == 0) {
+    Serial.println(F("turn grow light off"));
+    MCUBus.reply("ok", 2);
+    return;
+  }
+
+  if (strcmp_P(payload_str, (PGM_P)F("air_temp_f")) == 0) {
+    // For some reason I couldn't get String to work
+    // Also fuck arduino's sprintf doesn't support floats
+
+    float tempf = isnan(air_temp_f) ? 0.0 : air_temp_f;
+    char myConcatenation[20];
+
+    sprintf(myConcatenation,"air_temp_f=%i.0", (int)round(tempf));
+    Serial.print(F("Replying with: "));
+    Serial.println(myConcatenation);
+
+    MCUBus.reply(myConcatenation, 13);
+    return;
+  }
+}
+
+void onBusPacket(uint8_t *payload, uint16_t length, const PacketInfo &packet_info) {
+  char payload_str[length+1];  // +1 for null terminator
+  for(uint16_t i = 0; i < length; ++i) {
+    payload_str[i] = (char)payload[i];
+  }
+  payload_str[length] = '\0'; // null terminate the string
+
+  Serial.print(F("Received "));
+  Serial.print(length);
+  Serial.print(F(" bytes: "));
+  Serial.println(payload_str);
+
+  payloadRouter(payload_str);
+}
 
 void setup() {
   Serial.begin(74880);
-  Serial.println("Ready to receive");
+  Serial.println(F("Ready to receive"));
 //   // pinMode(GROW_LIGHT_PIN, OUTPUT);
 //
 //
   MCUBus.strategy.set_pin(MCU_BUS_PIN);
   MCUBus.begin();
-  MCUBus.set_receiver([](uint8_t *payload, uint16_t length, const PacketInfo &packet_info) {
-
-    String payload_str;
-    for(uint16_t i = 0; i < length; ++i) {
-      payload_str += (char)payload[i];
-    }
-
-    Serial.print("Received ");
-    Serial.print(length);
-    Serial.print(" bytes: ");
-    Serial.println(payload_str);
-
-    // if (payload_str == "water_level") {
-    //   String reply_str = "water_level=1.69";
-    //   Serial.print("Responding with: ");
-    //   Serial.println(reply_str.c_str());
-    //   MCUBus.reply(reply_str.c_str(), reply_str.length());
-    //   // return;
-    // }
-
-    // if (payload_str == "grow_light_on") {
-      // Serial.println("turn grow light on");
-      // MCUBus.reply("ok", 2);
-      // return;
-    // }
-
-    // if (payload_str == "grow_light_off") {
-      // Serial.println("turn grow light off");
-      // MCUBus.reply("ok", 2);
-      // return;
-    // }
-    //
-    if (payload_str == "air_temp_f") {
-      // For some reason I couldn't get String to work
-      // Also fuck arduino's sprintf doesn't support floats
-
-      float tempf = isnan(air_temp_f) ? 0.0 : air_temp_f;
-      char myConcatenation[20];
-
-      sprintf(myConcatenation,"air_temp_f=%i.0", (int)round(tempf));
-      Serial.print("Replying with: ");
-      Serial.println(myConcatenation);
-
-      MCUBus.reply(myConcatenation, 13);
-      return;
-    }
-  });
+  MCUBus.set_receiver(onBusPacket);
 
 };
 
@@ -87,13 +91,13 @@ void readTemp()
     case DHTLIB_OK:
 		break;
     case DHTLIB_ERROR_CHECKSUM:
-		Serial.print("DHT11 Checksum error,\t");
+		Serial.print(F("DHT11 Checksum error,\t"));
 		break;
     case DHTLIB_ERROR_TIMEOUT:
-		Serial.print("DHT11 Time out error,\t");
+		Serial.print(F("DHT11 Time out error,\t"));
 		break;
     default:
-		Serial.print("DHT11 Unknown error,\t");
+		Serial.print(F("DHT11 Unknown error,\t"));
 		break;
   }
   // DISPLAY DATA
@@ -115,7 +119,7 @@ void loop() {
   if (millis() - tempf_last_read >= 2000) {
     tempf_last_read = millis();
     readTemp();
-    Serial.print("air_temp_f: ");
+    Serial.print(F("air_temp_f: "));
     Serial.println(air_temp_f);
     // air_temp_f = dht.readTemperature(true);
   }
